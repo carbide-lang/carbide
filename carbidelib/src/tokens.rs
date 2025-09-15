33f0dd9532db3bdfea3282a925adc32ebc6605ec
languint <longuint@proton.me>
2025-09-15 09:47:24 -0500

    feat(lib): add primitive tokens

--- carbidelib/src/tokens.rs
+++ carbidelib/src/tokens.rs
@@ -1,1 +1,31 @@
+use logos::Logos;
 
+#[derive(Debug, Logos, PartialEq, Clone)]
+#[logos(skip r"[ \t\n\f]+")]
+pub enum Tokens {
+    // Primitives
+    #[regex(r"[0-9]+", |lex| lex.slice().parse::<i64>().ok())]
+    Integer(i64),
+    #[regex(r"[0-9]+\.[0-9]+", |lex| lex.slice().parse::<f64>().ok())]
+    Float(f64),
+    #[regex(r#""([^"\\]|\\[nrt"\\])*""#, |lex| {
+        let slice = lex.slice();
+
+        let content = &slice[1..slice.len()-1];
+        Some(content.replace("\\n", "\n")
+                   .replace("\\r", "\r")
+                   .replace("\\t", "\t")
+                   .replace("\\\"", "\"")
+                   .replace("\\\\", "\\"))
+    })]
+    String(String),
+    #[regex(r"true|false", |lex| match lex.slice() {
+        "true" => Some(true),
+        "false" => Some(false),
+        _ => None,
+    })]
+    Boolean(bool),
+    #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*", |lex| Some(lex.slice().to_string()))]
+    Identifier(String),
+    
+}
