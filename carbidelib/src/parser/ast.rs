69b6a704b13b98ea4474835496425b203fa9fa58
languint <longuint@proton.me>
2025-09-16 08:51:20 -0500

    feat(ast): add token traversing functions

--- carbidelib/src/parser/ast.rs
+++ carbidelib/src/parser/ast.rs
@@ -12,3 +12,38 @@ impl TryFrom<Vec<Tokens>> for AST {
 
     type Error = ASTError;
 }
+
+impl AST {
+    pub fn peek(&self) -> Option<&Tokens> {
+        return self.tokens.get(self.index + 1);
+    }
+
+    pub fn consume_if_eq(&mut self, token: &Tokens) -> bool {
+        if let Some(t) = self.peek() {
+            if std::mem::discriminant(t) == std::mem::discriminant(token) {
+                self.advance();
+                return true;
+            }
+        }
+        false
+    }
+
+    fn advance(&mut self) -> Option<&Tokens> {
+        if !self.is_at_end() {
+            self.index += 1;
+        }
+        self.previous()
+    }
+
+    fn previous(&self) -> Option<&Tokens> {
+        if self.index > 0 {
+            self.tokens.get(self.index - 1)
+        } else {
+            None
+        }
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.index >= self.tokens.len()
+    }
+}
